2926861090c3cf633d6a6fd6303d3493dae163bf
Kirill <72662821+callbackcat@users.noreply.github.com>
2024-09-30 22:27:56 +0300

    build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
-  output:  process.env.NODE_ENV !== "production" ? undefined: "export", 
-  images: {
-    unoptimized: envImageUnoptimize,
-    remotePatterns: [
-      {
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
