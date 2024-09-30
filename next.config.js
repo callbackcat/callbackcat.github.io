27a7f21953c9a67151deba7e66077bd98476e0f4
Kirill <72662821+callbackcat@users.noreply.github.com>
2024-09-30 22:36:00 +0300

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig;
